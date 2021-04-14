cd34f8d4947d6d8fadaf103d80a6a9d1a572ec31
EibboR-dev <82501743+EibboR-dev@users.noreply.github.com>
2021-04-14 16:16:53 +0800

    Add files via upload

---
+++ Personal-Info.py
@@ -0,0 +1,8 @@
+firstName = str( input("Input Your First Name: "))
+lastName = str( input("Input Your Last Name: "))
+strLocation = str( input ("Input Your Location: "))
+intAge = int ( input("Input Your Age: "))
+
+varSpace = " "
+
+print("You are " + firstName + varSpace + lastName + varSpace + ". You are from " + strLocation + ". You are " + str(intAge) + " years old.")
